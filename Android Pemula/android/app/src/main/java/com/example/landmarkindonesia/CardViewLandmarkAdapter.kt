de43f43f194db5c100c14695b1dfade384b08fe7
aftermath089 <vincentmichael089@gmail.com>
2019-11-08 13:13:20 +0800

    activity about to scrollview on landscape

--- Android Pemula/android/app/src/main/java/com/example/landmarkindonesia/CardViewLandmarkAdapter.kt
+++ Android Pemula/android/app/src/main/java/com/example/landmarkindonesia/CardViewLandmarkAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -26,6 +27,10 @@ class CardViewLandmarkAdapter(private val listLandmark: ArrayList<LandmarkModel>
 
         Glide.with(holder.itemView.context).load(landmark.photo).apply(RequestOptions().override(350,550)).into(holder.imgPhoto)
 
+        holder.itemView.setOnClickListener{
+            Toast.makeText(holder.itemView.context, landmark.name, Toast.LENGTH_LONG).show()
+        }
+
     }
 
     inner class CardViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -33,4 +38,5 @@ class CardViewLandmarkAdapter(private val listLandmark: ArrayList<LandmarkModel>
         var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
         var tvDetail: TextView = itemView.findViewById(R.id.tv_item_detail)
     }
+
 }
