3615105ab1c091bfb99422a635dc7906f43ad4da
aftermath089 <vincentmichael089@gmail.com>
2020-01-02 15:20:30 +0800

    add countdown timer

--- Udacity Android/Architecture and Lifecycle/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ Udacity Android/Architecture and Lifecycle/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -17,6 +17,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -81,6 +82,10 @@ class GameFragment : Fragment() {
                 gameFinished()
                 viewModel.onGameFinishComplete()
         }
+        })
+
+        viewModel.currentTime.observe(this, Observer {
+            newTime -> binding.timerText.text = DateUtils.formatElapsedTime(newTime)
         })
 
         return binding.root
--- Udacity Android/Architecture and Lifecycle/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Udacity Android/Architecture and Lifecycle/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,11 +1,25 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel(){
+    companion object{
+        // These represent different important times
+        // This is when the game is over
+        const val DONE = 0L
+
+        // This is the number of milliseconds in a second
+        const val ONE_SECOND = 1000L
+
+        // This is the total time of the game
+        const val COUNTDOWN_TIME = 5000L
+    }
+
+    private val timer : CountDownTimer
 
     // The current word
     private val _word = MutableLiveData<String>()
@@ -22,6 +36,11 @@ class GameViewModel : ViewModel(){
     val eventGameFinish : LiveData<Boolean>
         get() = _eventGameFinish
 
+    // Current Time
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime : LiveData<Long>
+        get() = _currentTime
+
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
@@ -33,11 +52,25 @@ class GameViewModel : ViewModel(){
 
         _score.value = 0
 
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND){
+            override fun onFinish() {
+                _eventGameFinish.value = true
+                _currentTime.value = DONE
+            }
+
+            override fun onTick(p0: Long) {
+                _currentTime.value = p0/ ONE_SECOND
+            }
+
+        }
+
+        timer.start()
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel Destroyed!")
+        timer.cancel() //avoid memoryleaks
     }
 
     /**
@@ -78,11 +111,10 @@ class GameViewModel : ViewModel(){
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-            //gameFinished()
-            _eventGameFinish.value = true
-        } else {
-            _word.value = wordList.removeAt(0)
+            resetList()
         }
+        _word.value = wordList.removeAt(0)
+
     }
 
     /** Methods for buttons presses **/
