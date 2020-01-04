14cbe63207415b90d833d984ea03ce42afa4447b
aftermath089 <vincentmichael089@gmail.com>
2020-01-05 00:24:23 +0800

    add Data Access Object

--- Udacity Android/Database/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ Udacity Android/Database/andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,4 +16,30 @@
 
 package com.example.android.trackmysleepquality.database
 
-interface SleepDatabaseDao
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface SleepDatabaseDao{
+//inserting
+    @Insert
+    fun insert(night: SleepNight)
+//updating
+    @Update
+    fun update(night: SleepNight)
+//custom query
+    @Query("SELECT * FROM daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: Long): SleepNight
+
+    @Query("DELETE FROM daily_sleep_quality_table") //clear all data in table
+    fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>> //Live Data so data will autoupdate in UI by observing the database, no need to call multiple times
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight():SleepNight? //nullable because if table is cleared there is a possibilities that getTonight will return null value
+}
