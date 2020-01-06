e40a476bd50933583f053363b1823c2a0f4c4d25
aftermath089 <vincentmichael089@gmail.com>
2020-01-06 16:27:28 +0800

    RecycleView behavior

--- Udacity Android/RecyclerView/andfun-kotlin-sleep-tracker-with-recyclerview-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ Udacity Android/RecyclerView/andfun-kotlin-sleep-tracker-with-recyclerview-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
@@ -37,6 +38,11 @@ class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>(){
 
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
         val item = data[position]
+        if (item.sleepQuality <= 1) {
+            holder.textView.setTextColor(Color.RED) // if we dont put else then colour red will be recycled even when sleep quality is above 1
+        } else {
+            holder.textView.setTextColor(Color.BLACK)
+        }
         holder.textView.text = item.sleepQuality.toString()
     }
 
