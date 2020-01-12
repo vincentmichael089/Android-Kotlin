d8b4efba5120aa3adfbe0c0d7e97188967b48cf0
aftermath089 <vincentmichael089@gmail.com>
2020-01-13 04:11:38 +0800

    filter query

--- Udacity Android/Internet/andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ Udacity Android/Internet/andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -24,6 +24,7 @@ import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
@@ -40,7 +41,7 @@ private val moshi = Moshi.Builder()
 
 interface MarsApiService{
     @GET("realestate")
-    fun getProperties():
+    fun getProperties(@Query("filter")type: String):
             Deferred<List<MarsProperty>> //  Deffered is Coroutine return value
 }
 
@@ -48,4 +49,12 @@ object MarsApi{ // object so can be called in OverviewViewModel
     val retrofitService : MarsApiService by lazy{
         retrofit.create(MarsApiService::class.java)
     }
+}
+
+// FIltering
+enum class MarsApiFilter(val value : String){
+    SHOW_RENT("rent"),
+    SHOW_BUY("buy"),
+    SHOW_ALL("all")
+
 }
--- Udacity Android/Internet/andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ Udacity Android/Internet/andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -22,6 +22,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsApiFilter
 import com.example.android.marsrealestate.network.MarsProperty
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -65,15 +66,15 @@ class OverviewViewModel : ViewModel() {
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
     init {
-        getMarsRealEstateProperties()
+        getMarsRealEstateProperties(MarsApiFilter.SHOW_ALL)
     }
 
     /**
      * Sets the value of the status LiveData to the Mars API status.
      */
-    private fun getMarsRealEstateProperties() {
+    private fun getMarsRealEstateProperties(filter: MarsApiFilter) {
         coroutineScope.launch {
-            val getPropertiesDeffered = MarsApi.retrofitService.getProperties()
+            val getPropertiesDeffered = MarsApi.retrofitService.getProperties(filter.value)
             try {
                 _status.value = MarsApiStatus.LOADING
                 val listResult = getPropertiesDeffered.await()
@@ -101,5 +102,10 @@ class OverviewViewModel : ViewModel() {
 
     fun displayPropertyDetailsComplete(){
         _navigateToSelectedProperty.value = null
+    }
+
+    // Additional Filtering Function
+    fun updateFilter(filter: MarsApiFilter){
+        getMarsRealEstateProperties(filter)
     }
 }
