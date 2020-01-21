ef8f9942822eb03caa8d0aa5de5db9b427cb45c9
aftermath089 <vincentmichael089@gmail.com>
2020-01-21 20:28:40 +0800

    add DatabaseVideo Entity

--- Udacity Android/Caching/andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ Udacity Android/Caching/andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -17,3 +17,30 @@
 
 package com.example.android.devbyteviewer.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.android.devbyteviewer.domain.Video
+
+@Entity
+data class DatabaseVideo constructor(
+        @PrimaryKey
+        val url : String,
+        val updated : String,
+        val title : String,
+        val description :  String,
+        val thumbnail : String
+)
+/**
+* extension function which converts from database objects to domain objects
+*/
+fun List<DatabaseVideo>.asDomainModel() : List<Video>{
+    return map{
+        Video(
+                url = it.url,
+                title = it.title,
+                description = it.description,
+                updated = it.updated,
+                thumbnail = it.thumbnail
+        )
+    }
+}
