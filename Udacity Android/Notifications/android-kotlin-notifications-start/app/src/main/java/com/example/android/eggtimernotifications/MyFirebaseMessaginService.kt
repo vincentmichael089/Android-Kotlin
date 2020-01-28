9407cd2c3c8696d51b82590be6e88ed857b200a6
aftermath089 <vincentmichael089@gmail.com>
2020-01-28 18:28:17 +0800

    transform Data Messages with FCM to in App notification

--- Udacity Android/Notifications/android-kotlin-notifications-start/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessaginService.kt
+++ Udacity Android/Notifications/android-kotlin-notifications-start/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessaginService.kt
@@ -43,6 +43,10 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
         // TODO Step 3.6 check messages for notification and call sendNotification
         // Check if message contains a notification payload.
+        remoteMessage?.notification?.let {
+            Log.d(TAG, "Message Notification Body : "+it.body)
+            sendNotification(it.body!!)
+        }
 
     }
     // [END receive_message]
