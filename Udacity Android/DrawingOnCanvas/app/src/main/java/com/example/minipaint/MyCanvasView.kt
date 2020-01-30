bdc1e2a9d76784b680296c88c6e608d8b9f72e53
aftermath089 <vincentmichael089@gmail.com>
2020-01-31 05:14:09 +0800

    Override onSizeChanged()

--- Udacity Android/DrawingOnCanvas/app/src/main/java/com/example/minipaint/MyCanvasView.kt
+++ Udacity Android/DrawingOnCanvas/app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -1,8 +1,28 @@
 package com.example.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class MyCanvasView(context: Context) : View(context){
 
+    // bitmap and canvas for caching what has been drawn before.
+    private lateinit var extraCanvas : Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        // a new bitmap and canvas are created every time the function executes. You need a new bitmap,
+        // because the size has changed. However, this is a memory leak, leaving the old bitmaps around.
+        // To fix this, recycle extraBitmap before creating the next one.
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888) //ARGB_8888 stores each color in 4 bytes and is recommended.
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
 }
