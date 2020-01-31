7e72617c18eea22e36310daa61f48a819e901bc8
aftermath089 <vincentmichael089@gmail.com>
2020-02-01 05:52:17 +0800

    Create and Initialize a Paint and a Path Object

--- Udacity Android/Clipping/app/src/main/java/com/example/clippingexample/ClippedView.kt
+++ Udacity Android/Clipping/app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -1,6 +1,8 @@
 package com.example.clippingexample
 
 import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
@@ -10,4 +12,11 @@ class ClippedView @JvmOverloads constructor(
     defStyleAttr : Int = 0
 ) : View(context, attrs, defStyleAttr){
 
+    private val paint = Paint().apply{
+        isAntiAlias = true
+        strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+        textSize = resources.getDimension(R.dimen.textSize)
+    }
+
+    private val path = Path()
 }
