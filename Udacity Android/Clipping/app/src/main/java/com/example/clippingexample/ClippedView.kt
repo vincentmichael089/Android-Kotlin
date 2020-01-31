01f152a0ac5aff0ad5040873504b456ff925f7d9
aftermath089 <vincentmichael089@gmail.com>
2020-02-01 06:32:01 +0800

    Implement drawRoundedRectangleClippingExample

--- Udacity Android/Clipping/app/src/main/java/com/example/clippingexample/ClippedView.kt
+++ Udacity Android/Clipping/app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -43,6 +43,13 @@ class ClippedView @JvmOverloads constructor(
     private val rowFour = rowThree + rectInset + clipRectBottom
     private val textRow = rowFour + (1.5f * clipRectBottom)
 
+    private var rectF = RectF(
+        rectInset,
+        rectInset,
+        clipRectRight - rectInset,
+        clipRectBottom - rectInset
+    )
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         drawBackAndUnclippedRectangle(canvas)
@@ -202,6 +209,16 @@ class ClippedView @JvmOverloads constructor(
     }
 
     private fun drawRoundedRectangleClippingExample(canvas: Canvas) {
+        canvas.save()
+        canvas.translate(columnTwo,rowThree)
+        path.rewind()
+        path.addRoundRect(
+            rectF,clipRectRight / 4,
+            clipRectRight / 4, Path.Direction.CCW
+        )
+        canvas.clipPath(path)
+        drawClippedRectangle(canvas)
+        canvas.restore()
     }
 
     private fun drawOutsideClippingExample(canvas: Canvas) {
