a58da1f6a4b0ebaf3bdbf7b4abbd65fc9aae4ab2
aftermath089 <vincentmichael089@gmail.com>
2020-02-01 06:30:04 +0800

    Implement drawCombinedClippingExample

--- Udacity Android/Clipping/app/src/main/java/com/example/clippingexample/ClippedView.kt
+++ Udacity Android/Clipping/app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -182,6 +182,23 @@ class ClippedView @JvmOverloads constructor(
     }
 
     private fun drawCombinedClippingExample(canvas: Canvas) {
+        canvas.save()
+        canvas.translate(columnOne, rowThree)
+        path.rewind()
+        path.addCircle(
+            clipRectLeft + rectInset + circleRadius,
+            clipRectTop + circleRadius + rectInset,
+            circleRadius,Path.Direction.CCW
+        )
+        path.addRect(
+            clipRectRight / 2 - circleRadius,
+            clipRectTop + circleRadius + rectInset,
+            clipRectRight / 2 + circleRadius,
+            clipRectBottom - rectInset,Path.Direction.CCW
+        )
+        canvas.clipPath(path)
+        drawClippedRectangle(canvas)
+        canvas.restore()
     }
 
     private fun drawRoundedRectangleClippingExample(canvas: Canvas) {
