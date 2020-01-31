a5d6c467b5b00bc4a74e257d34accbcc66a96e2a
aftermath089 <vincentmichael089@gmail.com>
2020-02-01 05:58:06 +0800

    Set up the shapes

--- Udacity Android/Clipping/app/src/main/java/com/example/clippingexample/ClippedView.kt
+++ Udacity Android/Clipping/app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -19,4 +19,17 @@ class ClippedView @JvmOverloads constructor(
     }
 
     private val path = Path()
+
+    private val clipRectRight = resources.getDimension(R.dimen.clipRectRight)
+    private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
+    private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
+    private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
+
+    private val rectInset = resources.getDimension(R.dimen.rectInset)
+    private val smallRectOffset = resources.getDimension(R.dimen.smallRectOffset)
+
+    private val circleRadius = resources.getDimension(R.dimen.circleRadius)
+
+    private val textOffset = resources.getDimension(R.dimen.textOffset)
+    private val textSize = resources.getDimension(R.dimen.textSize)
 }
