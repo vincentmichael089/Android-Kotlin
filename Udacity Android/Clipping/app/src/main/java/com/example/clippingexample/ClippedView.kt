6ca23c44e26c47a4f0a1602a02a4800e53c4c2d5
aftermath089 <vincentmichael089@gmail.com>
2020-02-01 05:41:40 +0800

    Create the ClippingExample Project

---
+++ Udacity Android/Clipping/app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -0,0 +1,13 @@
+package com.example.clippingexample
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ClippedView @JvmOverloads constructor(
+    context : Context,
+    attrs : AttributeSet? = null,
+    defStyleAttr : Int = 0
+) : View(context, attrs, defStyleAttr){
+
+}
