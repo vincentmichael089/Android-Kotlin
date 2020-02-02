d9f15c3ad220ca412571090366042a75f50ef89c
aftermath089 <vincentmichael089@gmail.com>
2020-02-03 04:55:23 +0800

    rotate object

--- Udacity Android/Animation Properties/android-kotlin-animation-property-animation-master/android-kotlin-animation-property-animation-master/begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ Udacity Android/Animation Properties/android-kotlin-animation-property-animation-master/android-kotlin-animation-property-animation-master/begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -16,8 +16,12 @@
 
 package com.google.samples.propertyanimation
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 
@@ -70,7 +74,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rotater() {
+        val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360F, 0F)
+        animator.duration = 1000
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                rotateButton.isEnabled = false
+            }
+            override fun onAnimationEnd(animation: Animator?) {
+                rotateButton.isEnabled = true
+            }
+        })
+        animator.start()
     }
+
+    //Note: The reason that the animation starts at -360 is that that allows the star to complete a
+    // full circle (360 degrees) and end at 0, which is the default rotation value for a non-rotated
+    // view, so it’s a good value to have at the end of the animation (in case any other action occurs
+    // on that view later, expecting the default value).
+
+    //Note: A property, to the animation system, is a field that is exposed via setters and getters,
+    // either implicitly (as properties are in Kotlin) or explicitly (via the setter/getter pattern
+    // in the Java programming language). There are also a special case of properties exposed via the
+    // class android.util.Property which is used by the View class, which allows a type-safe approach
+    // for animations, as we’ll see later. The Animator system in Android was specifically written to
+    // animate properties, meaning that it can animate anything (not just UI elements) that has a setter
+    // (and, in some cases, a getter)
+    //
+    //Note: View properties are a set of functionality added to the base View class that allows all
+    // views to be transformed in specific ways that are useful in UI animations. There are properties
+    // for position (called “translation”), rotation, scale, and transparency (called “alpha”).
+    //
+    //There are actually two different ways to access the properties, by regular setter/getter pairs,
+    // like setTranslateX()/getTranslateX(), and by static android.util.Property objects, like
+    // View.TRANSLATE_X (an object that has both a get() and a set() method). This lesson primarily
+    // uses android.util.Propertyobjects, because it has less overhead internally along with better
+    // type-safety, but you can also use the setters and getters of any object, as you will see toward
+    // the end of the lab.
+    //
+    //Note: Property animation is, simply, the changing of property values over time. ObjectAnimator
+    // was created to provide a simple set-and-forget mechanism for animating properties. For example,
+    // you can define an animator that changes the “alpha” property of a View, which will alter the
+    // transparency of that view on the screen.
+    //
+
 
     private fun translater() {
     }
