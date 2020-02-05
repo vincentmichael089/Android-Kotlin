e1011e5f87f12c4f1959fcf682b19b9422e6a707
aftermath089 <vincentmichael089@gmail.com>
2020-02-06 00:42:07 +0800

    add picture overlay on map

--- Udacity Android/Google Maps/Wander/app/src/main/java/com/example/wander/MapsActivity.kt
+++ Udacity Android/Google Maps/Wander/app/src/main/java/com/example/wander/MapsActivity.kt
@@ -11,10 +11,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -57,6 +54,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel))
+
+//        val overlaySize = 100f
+//        val overlay = GroundOverlayOptions()
+//            .image(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)) //must be png
+//            .position(sydney, overlaySize)
+//        mMap.addGroundOverlay(overlay)
 
         setMapLongClick(mMap)
         setPoiClick(mMap)
