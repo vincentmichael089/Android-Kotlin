39eac8c049851f1c69931c79d46ed71499e41a01
aftermath089 <vincentmichael089@gmail.com>
2020-02-08 04:39:21 +0800

    clean code test live data

--- Udacity Android/Testing Basic/android-testing-starter_code/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ Udacity Android/Testing Basic/android-testing-starter_code/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -9,6 +9,7 @@ import com.example.android.architecture.blueprints.todoapp.Event
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.Matchers.*
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,14 +18,20 @@ import org.robolectric.annotation.Config
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [Config.OLDEST_SDK])
 class TasksViewModelTest{
+    // Subject under test
+    private lateinit var taskViewModel: TasksViewModel
+
+    // Executes each task synchronously using Architecture Components.
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
+    @Before
+    fun setupViewModel() {
+        taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
 
     @Test
     fun addNewTask(){
-        // GIVEN a new model, add dependencies so the viewModel can get the application context
-        val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
         // WHEN
         taskViewModel.addNewTask()
@@ -32,5 +39,18 @@ class TasksViewModelTest{
         // THEN
         val value = taskViewModel.newTaskEvent.getOrAwaitValue()
         assertThat(value.getContentIfNotHandled(), not(notNullValue()))
+    }
+
+    // This test should check that if you've set your filter type to show all tasks,
+    // that the Add task button is visible.
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+        // When the filter type is ALL_TASKS
+        taskViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        // Then the "Add task" action is visible
+        val value = taskViewModel.tasksAddViewVisible.getOrAwaitValue()
+        assertThat(value, `is`(true))
+
     }
 }
