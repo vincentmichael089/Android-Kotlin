3247f781526cbdc976ff45fe08a453e8f08e91c9
aftermath089 <vincentmichael089@gmail.com>
2020-02-08 04:25:54 +0800

    test live data 1

--- Udacity Android/Testing Basic/android-testing-starter_code/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ Udacity Android/Testing Basic/android-testing-starter_code/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,9 +1,15 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.Event
+import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.nullValue
 import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
@@ -11,12 +17,29 @@ import org.robolectric.annotation.Config
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [Config.OLDEST_SDK])
 class TasksViewModelTest{
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
 
     @Test
     fun addNewTask(){
         // GIVEN a new model, add dependencies so the viewModel can get the application context
         val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
-        // WHEN adding a new task
-        // THEN new task event is triggered
+
+        val observer = Observer<Event<Unit>>{}
+        try{
+            // observe the live data
+            taskViewModel.newTaskEvent.observeForever { observer }
+
+            // when adding new task
+            taskViewModel.addNewTask()
+
+            // then new task event is triggered
+            val value = taskViewModel.newTaskEvent.value
+            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
+        }finally {
+            // remove the observer whatever happened
+            taskViewModel.newTaskEvent.removeObserver(observer)
+        }
     }
 }
